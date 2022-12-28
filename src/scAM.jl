213b84e260ef2ba020737a6f9c4434a88a953dec
Abhishek Murti <62957069+murti-abhishek@users.noreply.github.com>
2022-12-28 15:02:48 -0800

    Add relevant dependencies

--- src/scAM.jl
+++ src/scAM.jl
@@ -1,10 +1,28 @@
 module scAM
 
-# Write your package code here.
+# first list all the dependencies
+
+using ForwardDiff # remove this later
+using MultivariateStats
+using Plots
+using DelimitedFiles
+using DataFrames
+using LinearAlgebra
+using Statistics
+using StatsPlots
+using UMAP
+using Clustering
+using NearestNeighbors
+using VegaLite
+using Random
+using XLSX
+using HypothesisTests
+
+# Write your package code here
+# then add all the files (include all the files that include internal + external functions)
 include("extra_file.jl")
 
-using ForwardDiff
-
+# amke sure to only export user functions [do not export iternal functions]
 export my_func, x_derivative
 
 end
