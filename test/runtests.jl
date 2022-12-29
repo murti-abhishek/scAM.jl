9f0e094bcb577caa97bd633ab8c7040e48d00218
Abhishek Murti <62957069+murti-abhishek@users.noreply.github.com>
2022-12-29 07:59:51 -0800

    Add tests to read matrices and normalize function

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,28 @@
 using scAM
 using Test
 
+using DelimitedFiles
+
 @testset "scAM.jl" begin
     # Write your tests here.
 
-    # Read the matrix
-    test_matrix = [[1,2,3] [4,5,6] [7,8,9]]
+    # Read the files and matrices
+    files = readdir("./test/data/")
+    file_path = string(pwd(), "/test/data/", files[1])
+    test_matrix = readdlm(file_path)
+    test_matrix = test_matrix[2:end, 2:end]
+    result = normalize_matrix(test_matrix, 10)
+    @test typeof(result) == Matrix{Float64}
+
+    file_path = string(pwd(), "/test/data/", files[2])
+    test_matrix = readdlm(file_path)
+    test_matrix = test_matrix[2:end, 2:end]
+    result = normalize_matrix(test_matrix, 10)
+    @test typeof(result) == Matrix{Float64}
+
+    file_path = string(pwd(), "/test/data/", files[3])
+    test_matrix = readdlm(file_path)
+    test_matrix = test_matrix[2:end, 2:end]
     result = normalize_matrix(test_matrix, 10)
     @test typeof(result) == Matrix{Float64}
 
